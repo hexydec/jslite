eacdea1d1949d4a39c857890615ff66ba99d67be
Hexydec <will@hexydec.com>
2021-08-14 18:03:18 +0100

    Added viewport meta tag to index.php, and updated the options column to be as small as it can.

--- index.php
+++ index.php
@@ -92,6 +92,7 @@ if (!empty($_POST['action'])) {
 <html>
 	<head>
 		<title>Hexydec JSLite Minifier</title>
+		<meta name="viewport" content="width=device-width,initial-scale=1.0" />
 		<style>
 			html, body {
 				margin: 0;
@@ -143,10 +144,11 @@ if (!empty($_POST['action'])) {
 				font-weight: bold;
 			}
 			.minify__options {
-				flex: 0 0 150px;
+				flex: 0 0 auto;
 				padding: 10px;
 				background: #003ea4;
 				color: #FFF;
+				overflow: auto;
 			}
 			.minify__options-list {
 				list-style: none;
